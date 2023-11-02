8a1cdb1a43bb5cf8836523cc972b5039f81da908
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-02 18:39:05 +0600

    Added singer style App css file and App js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 
 const number = 5555;
 const singer = {name: 'Atif Islam', job:', Singer'}
+const singer2 = {name: 'Eva Rahman', job:', Singer'}
+const singer3 = {name: 'Habib Wahid', job:', Singer'}
+const singerStyle= {
+  color: 'blue',
+  backgroundColor: 'yellow'
+}
+
 function App() {
   return (
     <div className="App">
@@ -14,7 +21,11 @@ function App() {
         </div>
         <div className="music">
           <p>Number: {2222 + number}</p>
-          <p>Singer: {singer.name} {singer.job}</p>
+          <div className="singer">
+          <p>Singer: {singer.name} {singer.job}</p></div>
+          <p style={singerStyle}>Singer2: {singer2.name} {singer2.job}</p>
+          <p style={{ color: 'red',
+  backgroundColor: 'white'}}>Singer3: {singer3.name} {singer3.job}</p>
         </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
